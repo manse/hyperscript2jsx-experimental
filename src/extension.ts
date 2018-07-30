31373a16d93749c54a1845e953450888a4a9ff10
manse <info@manse.jp>
2018-07-30 21:03:10 +0900

    Implements converter (wip)

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,127 @@
 'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import * as prettier from 'prettier';
+import * as ts from 'typescript';
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const print = (() => {
+  const printer = ts.createPrinter({
+    newLine: ts.NewLineKind.LineFeed
+  });
+  const resultFile = ts.createSourceFile(
+    '/tmp/hyperscript2jsx-ppoi-chunk.ts',
+    '',
+    ts.ScriptTarget.Latest,
+    false,
+    ts.ScriptKind.TS
+  );
+  return (node: any) => printer.printNode(ts.EmitHint.Unspecified, node, resultFile);
+})();
+
+function process(node: any, inBrackets = false): string {
+  switch (node.kind) {
+    case ts.SyntaxKind.CallExpression:
+      let tagName = node.expression.escapedText;
+      let selectorArg: any;
+      let propsArg: any;
+      let childrenArgs: any[];
+      if (
+        node.arguments[0].kind === ts.SyntaxKind.StringKeyword ||
+        node.arguments[0].kind === ts.SyntaxKind.StringLiteral ||
+        node.arguments[0].kind === ts.SyntaxKind.TemplateExpression ||
+        node.arguments[0].kind === ts.SyntaxKind.BinaryExpression ||
+        node.arguments[0].kind === ts.SyntaxKind.ExpressionStatement ||
+        node.arguments[0].kind === ts.SyntaxKind.PropertyAccessExpression ||
+        node.arguments[0].kind === ts.SyntaxKind.ConditionalExpression
+      ) {
+        selectorArg = node.arguments.shift();
+      }
+      if (node.arguments[0].kind === ts.SyntaxKind.ObjectLiteralExpression) {
+        propsArg = node.arguments.shift();
+      }
+      childrenArgs = node.arguments;
+
+      const props: any = {};
+      if (selectorArg) {
+        props.className = print(selectorArg);
+      }
+      if (propsArg) {
+        propsArg.properties.forEach((prop: any) => {
+          props[prop.name.escapedText] = print(prop.initializer);
+        });
+      }
+      if (tagName === 'h' && props.className) {
+        const matched = props.className.match(/^\w+/);
+        if (matched) {
+          tagName = matched[0];
+          props.className = props.className.replace(/^\w+/, '');
+        }
+      }
+      return `<${tagName} ${Object.keys(props)
+        .map(p => `${p}={${(props as any)[p]}}`)
+        .join(' ')}>${childrenArgs.map(child => process(child)).join('\n')}</${tagName}>`;
+    case ts.SyntaxKind.ArrayLiteralExpression:
+      return (node.elements.map((elem: any) => process(elem)) || []).join('\n');
+    case ts.SyntaxKind.StringLiteral:
+      return node.text || '';
+    case ts.SyntaxKind.TemplateExpression:
+    case ts.SyntaxKind.PropertyAccessExpression:
+    case ts.SyntaxKind.NullKeyword:
+    case ts.SyntaxKind.BooleanKeyword:
+    case ts.SyntaxKind.UndefinedKeyword:
+    case ts.SyntaxKind.ObjectKeyword:
+    case ts.SyntaxKind.StringKeyword:
+    case ts.SyntaxKind.TrueKeyword:
+    case ts.SyntaxKind.FalseKeyword:
+      if (inBrackets) {
+        return print(node);
+      } else {
+        return `{${print(node)}}`;
+      }
+    case ts.SyntaxKind.BinaryExpression:
+      return `{${process(node.left, true)} /* ${print(node.operatorToken)} */ ${process(node.right, true)}}`;
+    case ts.SyntaxKind.ConditionalExpression:
+      return `{${print(node.condition)} ? (${process(node.whenTrue, true)}) : (${process(node.whenFalse, true)})}`;
+    case ts.SyntaxKind.ExpressionStatement:
+    case ts.SyntaxKind.ReturnStatement:
+      return process(node.expression);
+    default:
+      if (node.escapedText) {
+        if (inBrackets) {
+          return node.escapedText;
+        } else {
+          return `{${node.escapedText}}`;
+        }
+      }
+      return '';
+  }
+}
+
 export function activate(context: vscode.ExtensionContext) {
-
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "hyperscript-jsx-transformer" is now active!');
-
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+  let disposable = vscode.commands.registerCommand('extension.transform-hyperscript-jsx', () => {
+    try {
+      const editor = vscode.window.activeTextEditor;
+      if (!editor) {
+        return;
+      }
+      const hyperscript = editor.document.getText(editor.selection);
+      const source = ts.createSourceFile('./chunk.ts', hyperscript, ts.ScriptTarget.ES2015);
+      const chunks: string[] = [];
+      ts.forEachChild(source, (node: ts.Node) => {
+        console.log(node);
+        chunks.push(process(node));
+      });
+      const rawJsx = chunks.join('\n');
+      const resultJsx =
+        prettier.format(rawJsx, {
+          semi: false,
+          parser: 'typescript'
+        }) || rawJsx;
+      console.log(resultJsx);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
