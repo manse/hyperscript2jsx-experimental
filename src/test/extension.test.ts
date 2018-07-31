9883a37baa5f85fd9e2cfd9100687f4bd1f1036d
manse <info@manse.jp>
2018-07-31 11:12:57 +0900

    Test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -2,7 +2,6 @@ import * as assert from 'assert';
 
 suite('Extension Tests', function() {
   test('Something 1', function() {
-    assert.equal(-1, [1, 2, 3].indexOf(5));
-    assert.equal(-1, [1, 2, 3].indexOf(0));
+    assert.ok(!!'Yeah');
   });
 });
